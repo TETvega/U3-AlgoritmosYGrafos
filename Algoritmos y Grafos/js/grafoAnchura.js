6fdf0013ab62b5d2066f8138333d866da0eaa91c
annerh3 <annerh3@gmail.com>
2024-04-10 17:16:37 -0600

    guardando avance (no hubo avance) --A

--- Algoritmos y Grafos/js/grafoAnchura.js
+++ Algoritmos y Grafos/js/grafoAnchura.js
@@ -1,3 +1,30 @@
+function bfs(grafo, vInicial) {
+    let visitados = {}; // Objeto para almacenar los nodos visitados
+    let cola = [vInicial]; // Cola para el recorrido en anchura
+
+    // Mientras haya nodos en la cola
+    while (cola.length > 0) {
+        // Sacamos el primer nodo de la cola
+        let verticeActual = cola[0];
+        cola = cola.slice(1); // Eliminamos el primer elemento de la cola
+        // Si el nodo actual no ha sido visitado
+        if (!visitados[verticeActual]) {
+            // Marcamos el nodo como visitado
+            visitados[verticeActual] = true;
+            // Mostramos el nodo actual o realizamos alguna operación con él
+            console.log(verticeActual);
+            // Obtenemos los nodos adyacentes al nodo actual
+            let adyacentes = grafo[verticeActual];
+            // Agregamos los nodos adyacentes a la cola (si no han sido visitados)
+            adyacentes.forEach(adyacente => {
+                if (!visitados[adyacente]) {
+                    cola.push(adyacente);
+                }
+            });
+        }
+    }
+}
+
 function dfs(grafo, vInicial, visitados = {}) {
     // Marcamos el nodo actual como visitado
     visitados[vInicial] = true;
@@ -15,6 +42,34 @@ function dfs(grafo, vInicial, visitados = {}) {
     });
 }
 
+
+
+// function dfs(grafo, vInicial) {
+//     let visitados = {}; // Objeto para almacenar los nodos visitados
+//     let pila = [vInicial]; // Pila para el recorrido en profundidad
+
+//     // Mientras haya nodos en la pila
+//     while (pila.length > 0) {
+//         // Sacamos el último nodo de la pila
+//         let verticeActual = pila.pop();
+//         // Si el nodo actual no ha sido visitado
+//         if (!visitados[verticeActual]) {
+//             // Marcamos el nodo como visitado
+//             visitados[verticeActual] = true;
+//             // Mostramos el nodo actual o realizamos alguna operación con él
+//             console.log(verticeActual);
+//             // Obtenemos los nodos adyacentes al nodo actual
+//             let adyacentes = grafo[verticeActual];
+//             // Agregamos los nodos adyacentes a la pila (si no han sido visitados)
+//             adyacentes.forEach(adyacente => {
+//                 if (!visitados[adyacente]) {
+//                     pila.push(adyacente);
+//                 }
+//             });
+//         }
+//     }
+// }
+
 // Ejemplo de uso
 let grafo = {
     'A': ['B', 'C'],
@@ -25,5 +80,9 @@ let grafo = {
     'F': ['C']
 };
 
+console.log("Recorrido en Anchura:");
+
+bfs(grafo, 'D'); // Comienza el recorrido desde el nodo 'D'
+
 console.log("Recorrido en Profundidad:");
-dfs(grafo, 'D'); // Comienza el recorrido desde el nodo 'D'
+dfs(grafo, 'D');
