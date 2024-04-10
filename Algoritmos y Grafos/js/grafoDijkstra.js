607471ed41f6bf342edd9b6c9df410ac35b4a063
Hector Vega <hectormartinez1vg@gmail.com>
2024-04-10 00:10:56 -0600

    ADD cargar grafo inicial , add 3 grafos distintos, button para mostrar tabla de grafos --H

--- Algoritmos y Grafos/js/grafoDijkstra.js
+++ Algoritmos y Grafos/js/grafoDijkstra.js
@@ -36,13 +36,89 @@ const posicionCirculos = [
                 { nombre: 'G', left: 250, top: 300, LINEAS: [
                 ]}
             ]
+        },
+        {
+            nombre: 'grafo2',
+            circulos: [
+                { nombre: 'A', left: 100, top: 150, LINEAS: [
+                    { goTo: 'B', peso: 1 },
+                    { goTo: 'E', peso: 2 }
+                ]},
+                { nombre: 'B', left: 290, top: 350, LINEAS: [
+                    { goTo: 'G', peso: 2 },
+                ]},
+                { nombre: 'C', left: 300, top: 220, LINEAS: [
+                    { goTo: 'D', peso: 1 },
+                    { goTo: 'G', peso: 1 }
+                ]},
+                { nombre: 'D', left: 310, top: 258, LINEAS: [
+                    { goTo: 'A', peso: 7 },
+                    { goTo: 'E', peso: 1 }
+                ]},
+                { nombre: 'E', left: 40, top: 300, LINEAS: [
+                    { goTo: 'C', peso: 4 },
+                ]},
+                { nombre: 'F', left: 600, top: 40, LINEAS: [
+                    { goTo: 'A', peso: 3 },
+                    { goTo: 'E', peso: 2 }
+                ]},
+                { nombre: 'G', left: 250, top: 100, LINEAS: [
+                    { goTo: 'A', peso: 4 },
+                    { goTo: 'E', peso: 2 }
+                ]}
+            ]
+        },
+        {
+            nombre: 'grafo3',
+            circulos: [
+                { nombre: 'A', left: 300, top: 250, LINEAS: [
+                    { goTo: 'G', peso: 1 },
+                    { goTo: 'F', peso: 3 }
+                ]},
+                { nombre: 'B', left: 390, top: 359, LINEAS: [
+                    { goTo: 'C', peso: 4 },
+                    { goTo: 'E', peso: 6 },
+                ]},
+                { nombre: 'C', left: 40, top: 20, LINEAS: [
+                    { goTo: 'D', peso: 9 },
+                ]},
+                { nombre: 'D', left: 100, top: 100, LINEAS: [
+                    { goTo: 'F', peso: 1 },
+                    { goTo: 'E', peso: 3 }
+                ]},
+                { nombre: 'E', left: 40, top: 300, LINEAS: [
+                    { goTo: 'C', peso: 4 },
+                ]},
+                { nombre: 'F', left: 600, top: 40, LINEAS: [
+                    { goTo: 'E', peso: 2 }
+                ]},
+                { nombre: 'G', left: 280, top: 170, LINEAS: [
+                    { goTo: 'E', peso: 2 }
+                ]}
+            ]
         }
 
 ];
 
+// ========================= FUNCION PARA CARGAR EL GRAFO INICIAL ===========
+let grafoIsInicializate = false
+const cargarGrafoInicial = async() => {
+    if (!grafoIsInicializate) {
+        cargarGrafo('grafo1')
+        mostrarTabla('grafo1')
+        grafoIsInicializate=true
+    }
+        
+}
+
+
+window.addEventListener("load", async () => {
+    await cargarGrafoInicial()
+});
+
+
+// ==================== CARGANDO TODOS LOS ADD EVEN LISTENERS ================
 cargarListeners()
-
-
 function cargarListeners() {
     
     
@@ -72,11 +148,15 @@ function mostrarGrafo(e) {
         case seleccion === 'Grafo2':
 
              mandarResultado('Se cargara el Grafo #2')
+             cargarGrafo('grafo2')
+             mostrarTabla('grafo2')
         break;
 
         // caso 3 Grafo #3
         case seleccion === 'Grafo3':
             mandarResultado('Se cargara el Grafo #3')
+            cargarGrafo('grafo3')
+            mostrarTabla('grafo3')
         
         break;
         // Caso 4 Grafo creado 
@@ -97,6 +177,8 @@ function mostrarGrafo(e) {
 
 function cargarGrafo( grafo) {
 
+    contenidoCanvas.innerHTML = ''
+    tableBody_nodos.innerHTML = ''
     const circulos1 = posicionCirculos.find(item => item.nombre === grafo);
     grafo = circulos1.circulos
     circulos1.circulos.forEach(circulo => {
@@ -133,7 +215,9 @@ function cargarGrafo( grafo) {
                     // largo de la linea o camino que necesita recorrer 
                     // UTILIZA la formula de la Hipotenusa 
 
-                    // Si pasaron 110 y 110 con rajo le entienden
+                    // Si pasaron 110 y 111 con rajo le entienden
+                    // es la de la Hipotenusa basicamente 
+                    // pero los puntos son mediante funciones lineales y usa ** ta la potencia 
                 lineaElement.style.width = `${Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)}px`;
 
                 // /Punto de partida de la linea 
