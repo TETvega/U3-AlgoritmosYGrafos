f3a46db93e54564c05b6943f37b75113fe74df28
Hector Vega <hectormartinez1vg@gmail.com>
2024-04-25 00:16:08 -0600

    fix and stetic code para cargar grafos

--- Algoritmos y Grafos/js/grafoPrim.js
+++ Algoritmos y Grafos/js/grafoPrim.js
@@ -1,21 +1,28 @@
-import { crearLineasPorNodo, crearNodos } from "./helpers/CreateGrafos"
+import { esperar ,conexionYaExiste, crearLineasPorNodo, crearNodos } from "./helpers/CreateGrafos.js"
+
 
 
 // exportacion de funcioes 
 
 // Varbles Globales 
 let grafoIsInicializate= false
-
+let conexionNodal = []
 const Grafos = [
 {
   nombre: "Grafo1",
   tipo: "Bidirecional",
-  conexiones: {
-      "A:B": 2,
-      "A:C": 4,
-      "B:D": 6,
-      "B:F": 3,
-  },
+  conexiones:[
+    {A:{B:2, C:3 }},
+    {B:{A:2, E:5 , F:6 }},
+    {C:{A:3, E:2 , D:1 }},
+    {D:{C:1, G:1 , K:3 }},
+    {E:{B:5, C:2 , F:9 , H:2 }},
+    {F:{B:6, E:9 , G:2 }},
+    {G:{D:1, F:2 , K:3 , H:4 }},
+    {H:{I:1, J:20 }},
+    {K:{I:3, J:3 }},
+      
+  ],
   nodos: [
     {
       nodo: "A",
@@ -78,6 +85,36 @@ window.addEventListener("load", async () => {
 });
 
 
+cargarEvenListeners()
+
+function cargarEvenListeners() {
+  
+
+
+  
+  addEventListener('click' , grafoMinimoDeExpancion)
+}
+
+
+async function grafoMinimoDeExpancion(e) {
+  
+  if (e.target.classList.contains('circle')) {
+
+    console.log(e.target)
+    pasarNodosEstadoNoVisitado()
+
+    await esperar(300)
+
+
+
+  }
+}
+
+
+
+
+
+
 function cargarGrafo(grafo) {
   contenidoCanvas.innerHTML = ''
   // tableBody_nodos.innerHTML = ''
@@ -90,9 +127,60 @@ function cargarGrafo(grafo) {
       return
   }
   crearNodos(infoGrafo.nodos)
-  leerConexiones
+  crearConexionesNodales(infoGrafo.conexiones)
 
 
 }
-    
-   
+
+
+
+function crearConexionesNodales(conexiones) {
+  conexiones.forEach(nodo => procesarConexionNodal(nodo))
+}
+
+function procesarConexionNodal(nodo) {
+    for (let nodoActual in nodo) {
+      // comparando si el objeto es igial ala llave 
+      if (nodo.hasOwnProperty(nodoActual)) {
+        partirConexionNodal(nodoActual,nodo)
+      }
+    }
+}
+
+function partirConexionNodal(nodoActual, nodo) {
+  let nodoOrigen = nodoActual; // apuntamos al nodo actual
+  let conexionesActuales = nodo[nodoActual]; // obtenemos las conexiones
+  for (let nodoApuntado in conexionesActuales) {
+    if (conexionesActuales.hasOwnProperty(nodoApuntado)) {
+      let nodoDestino = nodoApuntado; // Almacena el nodo apuntado
+      let peso = conexionesActuales[nodoApuntado]; // Almacena el peso de la conexión
+
+      // obtencion de los elementos del html 
+      let nodoPrimario = document.getElementById(nodoOrigen);
+      let nodoSecundario = document.getElementById(nodoDestino);
+
+      // creacion del objeto 
+      const conexion = {
+        nodo: nodoOrigen,
+        nodoDestino: nodoDestino,
+      }
+      if (!conexionYaExiste(nodoDestino, nodoOrigen, conexionNodal)) {
+        conexionNodal.push(conexion);
+        crearLineasPorNodo(nodoPrimario, nodoSecundario);
+      }
+    }
+  }
+}
+
+
+
+// funciones para colores 
+function pasarNodosEstadoNoVisitado() {
+  const nodeArray = document.querySelectorAll('.circle')
+  const arrayNodos = [...nodeArray]
+  // console.log(arrayNodos);
+  arrayNodos.forEach( nodo => {
+    nodo.classList.remove('bg-warning');
+    nodo.classList.add('bg-secondary');
+  })
+}
--- Algoritmos y Grafos/js/helpers/CreateGrafos.js
+++ Algoritmos y Grafos/js/helpers/CreateGrafos.js
@@ -20,10 +20,11 @@ export function crearLineasPorNodo( puntoPartida , puntoFinalizacion) {
     const lineaElement = document.createElement('div');
     lineaElement.classList.add('linea');
             // se agrega +15 para centrar correctamente la linea por el radio de los circulos que es R=15
-            const posicionCirculox1 = puntoPartida.left + 15; 
-            const posicionCirculoy1 = puntoPartida.top + 15;
-            const posicionCirculox2 = puntoFinalizacion.left + 15;
-            const posicionCirculoy2 = puntoFinalizacion.top + 15
+            const posicionCirculox1 = parseFloat(puntoPartida.style.left) + 15; 
+            // console.log(posicionCirculox1);
+            const posicionCirculoy1 = parseFloat(puntoPartida.style.top) + 15;
+            const posicionCirculox2 = parseFloat(puntoFinalizacion.style.left) + 15;
+            const posicionCirculoy2 = parseFloat(puntoFinalizacion.style.top) + 15
                 lineaElement.style.width = `${Math.sqrt(Math.pow( (posicionCirculox2 - posicionCirculox1),2 )  + Math.pow((posicionCirculoy2 - posicionCirculoy1) ,2))}px`;
 
             // /Punto de partida de la linea 
@@ -39,6 +40,25 @@ export function crearLineasPorNodo( puntoPartida , puntoFinalizacion) {
                 // θ (grados) = θ (radianes) * 180/π
             const angle = Math.atan2(posicionCirculoy2 - posicionCirculoy1, posicionCirculox2 - posicionCirculox1) * 180 / Math.PI;
             lineaElement.style.transform = `rotate(${angle}deg)`;
-
+            // console.log(lineaElement);
             contenidoCanvas.appendChild(lineaElement);
+}
+
+
+// Funcion que verifica si el nodo existe o no 
+export function conexionYaExiste(nodoDestino, nodoOrigen, conexionNodal) {
+    return conexionNodal.some(conexion => (
+        (conexion.nodo === nodoDestino && conexion.nodoDestino === nodoOrigen) ||
+        (conexion.nodo === nodoOrigen && conexion.nodoDestino === nodoDestino)
+    ))
+}
+
+
+
+
+// funcion que recibe un tiempo y hace que espere el usuario 
+export function esperar(tiempoMs) {
+    return new Promise(resolve => {
+        setTimeout(resolve, tiempoMs)
+    })
 }
